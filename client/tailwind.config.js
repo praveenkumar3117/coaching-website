dd4055232a941e80e06b3283b86125941da8b956
yamuprajapati05@gmail.com <yamuprajapati05@gmail.com>
2022-08-15 23:49:45 +0530

    minor changes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/components/Home/hero.js",
-    "./src/components/Home/content.js",
-    "./src/components/Home/director.js",
-    "./src/App.js",
-    "./src/components/Footer.js",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
